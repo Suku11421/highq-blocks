961decf9c7592fd823432eedc920b8bfccc8e26e
Suku11421 <developer11@executiveship.com>
2023-03-21 23:51:10 +0530

    fixed the openblock references

--- generators/arduino/esp32.js
+++ generators/arduino/esp32.js
@@ -2,7 +2,7 @@
  * Visual Blocks Language
  *
  * Copyright 2020 openblock.cc.
- * https://github.com/openblockcc/openblock-blocks
+ * https://github.com/openblockcc/highq-blocks
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
